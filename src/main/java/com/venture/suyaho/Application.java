0bf2c3bfff78c9eeb1f3176ca818547399c55da1
k168ww <k168ww@gmail.com>
2024-05-17 13:39:25 +0900

    기본 세팅

--- src/main/java/com/venture/suyaho/Application.java
+++ src/main/java/com/venture/suyaho/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SuyahoApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SuyahoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
