66cb76f8be52b9cd3a4647b4ddc19961a1b639f2
k168ww <k168ww@gmail.com>
2024-05-17 13:36:49 +0900

    Initial commit

---
+++ src/main/java/com/venture/suyaho/SuyahoApplication.java
@@ -0,0 +1,13 @@
+package com.venture.suyaho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuyahoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuyahoApplication.class, args);
+    }
+
+}
