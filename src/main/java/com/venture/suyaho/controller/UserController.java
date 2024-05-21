63ee92ca088584f729f448ba4371cb99fbfbd46f
k168ww <k168ww@gmail.com>
2024-05-21 15:22:28 +0900

    #19 UserController로 통합

--- src/main/java/com/venture/suyaho/controller/UserController.java
+++ src/main/java/com/venture/suyaho/controller/UserController.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class LoginController {
+public class UserController {
 
     private final UserService userService;
 
     @Autowired
-    public LoginController(UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
